17a037644a2c7c333dbed19754191395084864c9
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-22 19:11:29 +0300

    add models Question and Choice also use: 'python manage.py migrate' 'python manage.py makemigrations page_1' and again 'python manage.py migrate' to apply changes in DB (mydb.sql)

--- page_1/models.py
+++ page_1/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.Model):
+    """
+    'question_text' or first positional param is the name of column in DB,
+    'CharField' or 'DateTimeField' is the type of data in this column
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date of publication')
+
+
+class Choice(models.Model):
+    """ What is question???"""
+    question = models.ForeignKey(
+        Questions,
+        on_delete=models.CASCADE,
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
