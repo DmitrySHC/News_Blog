9e66f9c39dd1974cf47c5601304ed3454ef98a72
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-22 18:17:33 +0300

    Change SQLite to MySQL

--- page_1/urls.py
+++ page_1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index),    # path is '' because the "page_1\" part is discarded in firstDJANGO.urls
+    path('', index, 'index'),    # path is '' because "page_1\" is discarded in firstDJANGO.urls
 ]
