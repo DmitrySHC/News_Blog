f506d99efed6486a4e980ddf2c8770a0be696dd8
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-23 16:32:11 +0300

    add model News

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    # blank mean that field can be ''
+    created_at = models.DateTimeField('Date of publication', auto_now_add=True)
+    updated_at = models.DateTimeField('Date of update', auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    # all photos will be saved in dir 'photos/'
+    # and subdirs consonant with the year, month and day
+    is_published = models.BooleanField(default=True)
