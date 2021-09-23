ed1854e77f0868cc2f64d5641b29436ac321d630
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-23 20:33:25 +0300

    first output to browser

--- news/views.py
+++ news/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
-# Create your views here.
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+    return HttpResponse(res)
