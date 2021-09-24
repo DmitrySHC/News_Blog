3406a5173d5032599389c7bec3463e87a56b86a7
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-24 12:04:15 +0300

    some info about column in object News (correct names of column in admin/news/news)

--- news/models.py
+++ news/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField(blank=True)
+    title = models.CharField(verbose_name='Наименование', max_length=200)
+    content = models.TextField(verbose_name='Контент', blank=True)
     # blank mean that field can be ''
-    created_at = models.DateTimeField('Date of publication', auto_now_add=True)
-    updated_at = models.DateTimeField('Date of update', auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    created_at = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Обновлено', auto_now=True)
+    photo = models.ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d')
     # all photos will be saved in dir 'photos/'
     # and subdirs consonant with the year, month and day
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(verbose_name='Новость опубликована', default=True)
 
     def __str__(self):
         return self.title
