3ddf7274ccf1d8a10477a7ca898ac17654338060
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-24 19:53:37 +0300

    add new method 'get_category' into view.py (method will represent (by using category template) news of callable category)

--- news/views.py
+++ news/views.py
@@ -16,3 +16,19 @@ def index(request):
         template_name='news/index.html',
         context=context
     )
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'category': category,
+        'categories': categories,
+    }
+    return render(
+        request=request,
+        template_name='news/category.html',
+        context=context
+    )
