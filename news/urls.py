871b7926c9adcfb9147b9986c329eba97177500b
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-24 19:57:35 +0300

    add ref into news.urls (urlpatterns) with pattern 'category/<int:category_id>/' to call method 'get_category' with attribute 'category_id'

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, get_category
 
 urlpatterns = [
-    path('', index, ),    # path is '' because "news/" discarded in firstDJANGO.urls
+    path('', index, ),  # path is '' because "news/" discarded in firstDJANGO.urls
+    path('category/<int:category_id>/', get_category),
+    # '<int:category_id>' mean that after '/' will be integer object
+    # save it into var 'category_id' and pass by named parameter to called function 'get_category'
 ]
