8b8cb061a516549b94ce74b2f9135cefcadbe08a
Dmitry <onishchuk.dmitry@gmail.com>
2021-09-24 11:46:22 +0300

    fix in AppConfig names and add verbose_name = 'Новости' to correct name in admin page (on main page)

--- firstDJANGO/settings.py
+++ firstDJANGO/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.apps.Page1Config',
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
