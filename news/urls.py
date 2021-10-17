af8bba2a9f00863271e2985f83984281dbe71966
Dmitry <onishchuk.dmitry@gmail.com>
2021-10-17 17:53:56 +0300

    rename "firstDjango" --> "News_Blog" in files

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, get_category
 
 urlpatterns = [
-    path('', index, ),  # path is '' because "news/" discarded in firstDJANGO.urls
+    path('', index, ),  # path is '' because "news/" discarded in News_Blog.urls
     path('category/<int:category_id>/', get_category),
     # '<int:category_id>' mean that after '/' will be integer object
     # save it into var 'category_id' and pass by named parameter to called function 'get_category'
